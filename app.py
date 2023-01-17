e9d49ae11944bbc66ced6b96907ff98828b4c267
deadcomedian <ilya-sharov-2000@mail.ru>
2023-01-17 15:31:45 +0300

    init

--- app.py
+++ app.py
@@ -4,26 +4,29 @@ from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-from flask import Flask
+from flask import Flask, request
+from collections import OrderedDict
+import json
 
 app = Flask(__name__)
 
 
-def classificator(data_frame):  #Определяем функицю
-    for feature in list(data_frame.columns):      # Задаем итератор в рамках названий колонок
-        if data_frame[feature].dtype == 'O':      # Условие для признаков соответсвующих типу даных "Objekt"
-            data_frame[feature].replace(['Yes', 'No'], [1, 0], inplace=True)   # Замена значений 'Yes' и 'No' на 0 и 1
-            for iteration, value in enumerate(list(data_frame[feature].unique())):  # Итератор в рамках уникальных значений признака
-                if type(value) == str:         # Условие для замены нецифровых значений
-                    if data_frame[feature].nunique() > 2:  # Условие компенсации порядкового номера для
-                        iteration += 1                     # тех признаков, в которых не было значений 'Yes' и 'No'
-                    data_frame[feature].replace(value, iteration, inplace=True)  # Замена всех нецифровых значений на соответсвущй порядковый номер
+def classificator(data_frame):
+    for feature in list(data_frame.columns):
+        if data_frame[feature].dtype == 'O':
+            data_frame[feature].replace(['Yes', 'No'], [1, 0], inplace=True)
+            for iteration, value in enumerate(list(data_frame[feature].unique())):
+                if type(value) == str:
+                    if data_frame[feature].nunique() > 2:
+                        iteration += 1
+                    data_frame[feature].replace(value, iteration, inplace=True)
 
 
-@app.route('/predict')
-def predict():
+@app.route('/analyze', methods=['POST'])
+def analyze():
     # read data
-    data = pd.read_csv('~/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+    # data = pd.read_csv('~/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+    data = pd.read_csv(request.files['file'])
 
     # prepare data
     del data['customerID']
@@ -49,22 +52,29 @@ def predict():
     min_max_scaler = preprocessing.MinMaxScaler()
     X_train = min_max_scaler.fit_transform(X_train)
     X_test = min_max_scaler.transform(X_test)
+
     # create, train & predict model
     lr = LogisticRegression()
     model = lr.fit(X_train, y_train)
     prediction_test = model.predict(X_test)
+
     # examine the impact of every property from dataset
     weights = pd.Series(model.coef_[0], index=X.columns.values)
     weights = weights.sort_values(ascending=False)
-    # print results
+    weights = weights.to_dict(OrderedDict)
+
+    # obtain accuracy
     accuracy = metrics.accuracy_score(y_test, prediction_test)
-    print(accuracy)
-    print(weights)
-    result = '<h1>' + str(accuracy) + '\n'
-    for weight in weights:
-        result += str(weight) + "\n"
-    result += '</h1>'
-    return result
+
+    # return results
+    info = {
+        'Accuracy': accuracy,
+        'Weights': weights
+    }
+    #result = json.dumps(info, indent=4)
+
+    return info
+
 
 if __name__ == '__main__':
     app.run()
